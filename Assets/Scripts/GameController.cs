939b791f168fbc24d0fc19f0b67a7d424cdbf53c
Shayan <shayanrostamzade@Shayans-MacBook-Pro.local>
2022-02-04 21:58:04 +0330

    4th Feb

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.AI;
 
+/*
+Important stuff:
+    the number of animals and grass places are 8
+*/
 
 public class GameController : MonoBehaviour
 {
     // Checking section
     public bool isNight = false;
-    [SerializeField] private int maxNumAnimals = 8; 
+    [SerializeField] private int maxNumAnimals = 8;
     private int animalsNum = 0;
 
     // Audio
@@ -28,16 +32,36 @@ public class GameController : MonoBehaviour
     public Transform[] grassPlaces;
 
 
+
+    //public GameObject cow;
+    private Vector3 pos = new Vector3(0, 0, 0);
+
+
     private void Start()
     {
         // Music
         _audioSource = GetComponentInChildren<AudioSource>();
         PlayBackGrounMusic();
 
-        // Animation
+        // Instantiate(cow, spawnPos, Quaternion.identity);
+        // StartCoroutine(Spawn());
     }
 
-    private void Update() 
+    // private IEnumerator Spawn()
+    // {
+    //     yield return new WaitForSeconds(2f);
+    //     NavMeshHit hit;
+    //     if(NavMesh.SamplePosition(spawnPos, out hit, 2f, 0))
+    //     {
+    //         Vector3 pos = hit.position;
+    //         //cow.GetComponent<NavMeshAgent>().enabled = false;
+    //         cow.GetComponent<NavMeshAgent>().Warp(pos);
+    //         cow.transform.position = pos;
+    //         //cow.GetComponent<NavMeshAgent>().enabled = true;
+    //     }
+    // }
+
+    private void Update()
     {
         AnimalController();
     }
@@ -50,35 +74,55 @@ public class GameController : MonoBehaviour
     }
 
     private void AnimalController()
-    {   
+    {
+        // the number of animals and grass places is 8
+        // for(int i = 0; i < 8; i++)
+        // {
+        //     int animalRandomDestination = Random.Range(0, grassPlaces.Length - 1);
+        //     Instantiate(animals[i], grassPlaces[i].position, Quaternion.identity);
+        //     animals[i].GetComponent<NavMeshAgent>().SetDestination(grassPlaces[animalRandomDestination].position);
+        //     if(animals[i].transform.position != grassPlaces[animalRandomDestination].position)
+        //     {
+        //         animals[i].GetComponent<Animator>().SetBool("changeDestination", true);
+        //     }
+        //     else if(animals[i].transform.position == grassPlaces[animalRandomDestination].position)
+        //     {
+        //         animals[i].GetComponent<Animator>().SetBool("changeDestination", false);
+        //     }
+        // }
+
+
         if(animalsNum <= maxNumAnimals)
         {
             int randAnimal = Random.Range(0, animals.Length - 1);
-            int animalRandPlace = Random.Range(0, grassPlaces.Length - 1);
+            int animalRandomInitialPos = Random.Range(0, grassPlaces.Length - 1);
+            int animalRandomFinalPos = Random.Range(0, grassPlaces.Length - 1);
             GameObject chosenAnimal = animals[randAnimal];
-            Vector3 spawnPosition = grassPlaces[animalRandPlace].position;
+            Vector3 spawnPosition = grassPlaces[animalRandomInitialPos].position;
 
             // Instantiating animals
             chosenAnimal = Instantiate(chosenAnimal, spawnPosition, Quaternion.identity);
             animalsNum++;
 
-            // Setting the destination randomly between some grass places 
-            
-            chosenAnimal.GetComponent<NavMeshAgent>().Warp(spawnPosition);
-            chosenAnimal.GetComponent<NavMeshAgent>().SetDestination(spawnPosition);
-            
-            
-            
+            // Setting the destination randomly between some grass places
+            chosenAnimal.GetComponent<NavMeshAgent>().SetDestination(grassPlaces[animalRandomFinalPos].position);
+
             // Reached the destination
-            if(chosenAnimal.transform.position != grassPlaces[animalRandPlace].position)
+            if(chosenAnimal.transform.position != grassPlaces[animalRandomFinalPos].position)
             {
                 chosenAnimal.GetComponent<Animator>().SetBool("changeDestination", true);
             }
-            else if(chosenAnimal.transform.position == grassPlaces[animalRandPlace].position)
+            else if(chosenAnimal.transform.position == grassPlaces[animalRandomFinalPos].position)
             {
                 chosenAnimal.GetComponent<Animator>().SetBool("changeDestination", false);
             }
-       
-        } 
+
+        }
+    }//AnimalController
+
+    private void SetAnimalDestination()
+    {
+
     }
+
 }
